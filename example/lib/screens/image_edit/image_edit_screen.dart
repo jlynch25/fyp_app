70f867416d8f76ef432dac30756fd5cf618267fd
jlynch25 <38656343+jlynch25@users.noreply.github.com>
2021-05-11 12:28:01 +0100

    save image //TODO clear old image from chache

--- example/lib/screens/image_edit/image_edit_screen.dart
+++ example/lib/screens/image_edit/image_edit_screen.dart
@@ -3,11 +3,13 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
+import 'package:mylib_example/components/default_button.dart';
 import 'package:mylib_example/components/edit_image_args.dart';
 import 'package:mylib_example/screens/profile/profile_screen.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:bloc/bloc.dart';
 import 'package:crop_your_image/crop_your_image.dart';
+import 'package:path_provider/path_provider.dart';
 
 class EditPhotoScreen extends StatefulWidget {
   static String routeName = "/edit_photo";
@@ -62,48 +64,86 @@ class _EditPhotoScreenState extends State<EditPhotoScreen> {
             children: [
               Expanded(
                 child: Visibility(
-                  visible: _croppedData == null,
-                  child: Stack(
-                    children: [
-                      Crop(
-                        controller: _cropController,
-                        image: imageBytes,
-                        onCropped: (croppedData) {
-                          setState(() {
-                            _croppedData = croppedData;
-                            _isCropping = false;
-                          });
-                        },
-                        withCircleUi: _isCircleUi,
-                        initialSize: 0.5,
-                        maskColor: _isSumbnail ? Colors.white : null,
-                        cornerDotBuilder: (size, index) => _isSumbnail
-                            ? const SizedBox.shrink()
-                            : const DotControl(),
-                      ),
-                      Positioned(
-                        right: 16,
-                        bottom: 16,
-                        child: GestureDetector(
-                          onTapDown: (_) => setState(() => _isSumbnail = true),
-                          onTapUp: (_) => setState(() => _isSumbnail = false),
-                          child: CircleAvatar(
-                            backgroundColor:
-                                _isSumbnail ? Colors.blue.shade50 : Colors.blue,
-                            child: Center(
-                              child: Icon(Icons.crop_free_rounded),
+                    visible: _croppedData == null,
+                    child: Stack(
+                      children: [
+                        Crop(
+                          controller: _cropController,
+                          image: imageBytes,
+                          onCropped: (croppedData) {
+                            setState(() {
+                              _croppedData = croppedData;
+                              _isCropping = false;
+                            });
+                          },
+                          withCircleUi: _isCircleUi,
+                          initialSize: 0.5,
+                          maskColor: _isSumbnail ? Colors.white : null,
+                          cornerDotBuilder: (size, index) => _isSumbnail
+                              ? const SizedBox.shrink()
+                              : const DotControl(),
+                        ),
+                        Positioned(
+                          right: 16,
+                          bottom: 16,
+                          child: GestureDetector(
+                            onTapDown: (_) =>
+                                setState(() => _isSumbnail = true),
+                            onTapUp: (_) => setState(() => _isSumbnail = false),
+                            child: CircleAvatar(
+                              backgroundColor: _isSumbnail
+                                  ? Colors.blue.shade50
+                                  : Colors.blue,
+                              child: Center(
+                                child: Icon(Icons.crop_free_rounded),
+                              ),
                             ),
                           ),
                         ),
+                      ],
+                    ),
+                    replacement: Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Center(
+                            child: _croppedData == null
+                                ? SizedBox.shrink()
+                                : Image.memory(_croppedData!),
+                          ),
+                          DefaultButton(
+                              text: "Set Profile Picture",
+                              press: () async {
+                                if (_croppedData != null) {
+                                  // final tempDir = await getTemporaryDirectory();
+                                  final documentsDir =
+                                      await getApplicationDocumentsDirectory();
+                                  final dir = await new Directory(
+                                          '${documentsDir.path}/profile_pic/')
+                                      .create();
+                                  final exists =
+                                      await File('${dir.path}/user.png')
+                                          .exists();
+
+                                  if (exists) {
+                                    File file = File('${dir.path}/user.png');
+                                    await file.delete();
+                                  }
+                                  File file =
+                                      await new File('${dir.path}/user.png')
+                                          .create();
+
+                                  await file.writeAsBytes(_croppedData!);
+                                  imageCache!.evict(FileImage(file));
+
+                                  Navigator.pushNamed(
+                                      context, ProfileScreen.routeName);
+                                }
+                              })
+                        ],
                       ),
-                    ],
-                  ),
-                  replacement: Center(
-                    child: _croppedData == null
-                        ? SizedBox.shrink()
-                        : Image.memory(_croppedData!),
-                  ),
-                ),
+                    )),
               ),
               if (_croppedData == null)
                 Padding(
--- example/lib/screens/profile/components/profile_pic.dart
+++ example/lib/screens/profile/components/profile_pic.dart
@@ -1,13 +1,41 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:mylib_example/size_config.dart';
+import 'package:path_provider/path_provider.dart';
 
-class ProfilePic extends StatelessWidget {
+class ProfilePic extends StatefulWidget {
   const ProfilePic({
     Key? key,
   }) : super(key: key);
+  @override
+  _ProfilePicState createState() => _ProfilePicState();
+}
+
+class _ProfilePicState extends State<ProfilePic> {
+  late Directory documentsDir;
+  late bool image_exists;
+  late FileImage user_image;
+
+  @override
+  void initState() {
+    getDir();
+  }
+
+  void getDir() async {
+    imageCache!.clear();
+    final dir = await getApplicationDocumentsDirectory();
+    final exists = await File("${dir.path}/profile_pic/user.png").exists();
+    setState(() {
+      documentsDir = dir;
+      user_image = FileImage(File("${documentsDir.path}/profile_pic/user.png"));
+      image_exists = exists;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    // getDir();
     return SizedBox(
       height: 115,
       width: 115,
@@ -15,10 +43,14 @@ class ProfilePic extends StatelessWidget {
         clipBehavior: Clip.none,
         fit: StackFit.expand,
         children: [
-          CircleAvatar(
-            // backgroundImage: AssetImage("assets/images/Profile Image.png"),
-            backgroundColor: Colors.blueGrey,
-          ),
+          image_exists
+              ? CircleAvatar(
+                  backgroundImage: user_image,
+                  backgroundColor: Colors.blueGrey,
+                )
+              : CircleAvatar(
+                  backgroundColor: Colors.blueGrey,
+                ),
           // Positioned(
           //   right: -16,
           //   bottom: 0,
