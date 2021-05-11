b91ed46f9c34a415dba8273355a7a008780b7fee
jlynch25 <38656343+jlynch25@users.noreply.github.com>
2021-05-11 13:39:51 +0100

    update user details

--- example/lib/screens/edit_profile/components/body.dart
+++ example/lib/screens/edit_profile/components/body.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:grpc/grpc.dart';
 import 'package:mylib/mylib.dart';
+import 'package:mylib_example/components/default_button.dart';
 import 'package:mylib_example/components/edit_image_args.dart';
 import 'package:mylib_example/protos/service.pb.dart';
 import 'package:mylib_example/screens/friend_list/friend_list.dart';
 import 'package:mylib_example/screens/image_edit/image_edit_screen.dart';
 import 'package:mylib_example/screens/profile/components/profile_menu.dart';
+import 'package:mylib_example/screens/profile/profile_screen.dart';
 // import 'package:mylib_example/screens/friend_list/friend_list.dart';
 import 'package:mylib_example/service/chat_service.dart';
 import 'package:mylib_example/size_config.dart';
@@ -23,10 +25,11 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  User user = ChatService.user;
+  User userOLD = ChatService.user;
   String updateResult = "false";
-  String blockchainResult = "false";
-  String userBalance = "0";
+  User user = User();
+  late String displayName;
+  late String phoneNumber;
 
   late File _image;
   final picker = ImagePicker();
@@ -47,7 +50,7 @@ class _BodyState extends State<Body> {
               child: Text(
                 "\t\t\t\tChange Profile Picture",
                 style: TextStyle(
-                    color: Colors.black.withOpacity(0.5), fontSize: 18),
+                    color: Colors.black.withOpacity(0.5), fontSize: 25),
                 textAlign: TextAlign.left,
               ),
             ),
@@ -90,22 +93,106 @@ class _BodyState extends State<Body> {
               child: Text(
                 "\n\t\t\t\tEdit Profile Details",
                 style: TextStyle(
-                    color: Colors.black.withOpacity(0.5), fontSize: 18),
+                    color: Colors.black.withOpacity(0.5), fontSize: 25),
                 textAlign: TextAlign.left,
               ),
             ),
           ),
-          ProfileMenu(
-            text: "My Account",
-            icon: Icon(
-              Icons.account_circle,
-              size: getProportionateScreenWidth(18),
-            ),
-            press: () {},
-            // press: () => {_buildDialog(context, user.wallets.toString())},
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(children: [
+              SizedBox(height: getProportionateScreenHeight(30)),
+              buildDisplayNameFormField(),
+              SizedBox(height: getProportionateScreenHeight(30)),
+              buildPhoneNumberFormField(),
+              SizedBox(height: getProportionateScreenHeight(40)),
+              DefaultButton(
+                  text: "continue",
+                  press: () async {
+                    if (user.phoneNumber.isNotEmpty || user.name.isNotEmpty) {
+                      try {
+                        await widget.service
+                            .updateUser(user)
+                            .then((val) => setState(() {
+                                  updateResult = val!;
+                                }));
+                      } on GrpcError catch (err) {
+                        _buildDialog(context, err.message!);
+                      }
+
+                      if (updateResult == "true") {
+                        // if all are valid then go to success screen
+                        final snackBar = SnackBar(
+                          content: Text('Profile Data Updated'),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        Navigator.pushNamed(context, ProfileScreen.routeName);
+                      }
+                    } else {
+                      final snackBar = SnackBar(
+                        content: Text('There is no data to change'),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                  })
+            ]),
           ),
         ],
       ),
     );
   }
+
+  TextFormField buildPhoneNumberFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.phone,
+      onChanged: (value) {
+        user.phoneNumber = value;
+      },
+      decoration: InputDecoration(
+        labelText: "Phone Number",
+        hintText: "${userOLD.phoneNumber}",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(
+          Icons.phone,
+          size: getProportionateScreenWidth(18),
+        ), //CustomSurffixIcon(svgIcon: "assets/icons/Phone.svg"),
+      ),
+    );
+  }
+
+  TextFormField buildDisplayNameFormField() {
+    return TextFormField(
+      onChanged: (value) {
+        user.name = value;
+      },
+      decoration: InputDecoration(
+        labelText: "Your Name",
+        hintText: "${userOLD.name}",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(
+          Icons.person,
+          size: getProportionateScreenWidth(18),
+        ), //CustomSurffixIcon(svgIcon: "assets/icons/User.svg"),
+      ),
+    );
+  }
+}
+
+Widget? _buildDialog(BuildContext context, String err) {
+  showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (_) => AlertDialog(title: Text(err), actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ]));
+  return null;
 }
